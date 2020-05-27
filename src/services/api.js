f31bb8bee0fc8c3c5adcfb3d8bc39e64f3ef1c59
tyagolp <tyagolp@gmail.com>
2020-05-27 11:00:56 -0300

    change baseURL to production

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://api.trendsis.com.br:3333/',
+  baseURL: 'https://api.trendsis.com.br:3333/',
   // baseURL: 'http://localhost:3334/', // debug celular
-  baseURL: 'http://10.0.3.2:3334/', // debug emulador
+  // baseURL: 'http://10.0.3.2:3334/', // debug emulador
 });
 
 export default api;
